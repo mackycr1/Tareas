4cf8c74990441ea792003888c901763d9672ccca
mackycr1 <mackycr1@gmail.com>
2021-10-01 20:42:10 -0600

    Created Tipo Identificacion Table and Script Configuration

---
+++ InitialData/ScriptConfiguration/TipoIdentificacion.sql
@@ -0,0 +1,47 @@
+﻿DROP TABLE IF EXISTS #TipoIdnetificacionTemp
+
+SELECT IdTipoIdentificacion, Descripcion
+INTO #TipoIdnetificacionTemp
+FROM
+(
+    VALUES 
+    (1, 'Cedula'),
+    (2, 'Pasaporte '),
+    (3, 'Cedula Juridica ')
+) AS TEMP (IdTipoIdentificacion, Descripcion)
+
+/******************************************************************************
+UPDATE : Upades any existing data
+******************************************************************************/
+UPDATE TI 
+SET TI.Descripcion = TITEMP.Descripcion
+FROM [dbo].[TipoIdentificacion] AS TI
+INNER JOIN #TipoIdnetificacionTemp AS TITEMP
+ON TI.IdTipoIdentificacion = TITEMP.IdTipoIdentificacion
+
+
+/******************************************************************************
+INSERT : Inserts any record that does not exist
+******************************************************************************/
+SET IDENTITY_INSERT [dbo].[TipoIdentificacion] ON
+
+INSERT INTO [dbo].[TipoIdentificacion]
+(
+    IdTipoIdentificacion,
+    Descripcion
+)
+SELECT IdTipoIdentificacion, 
+       Descripcion
+FROM #TipoIdnetificacionTemp
+
+
+/******************************************************************************
+SELECT : Select the data we inserted or updated on the Tipo Identificacion Table
+******************************************************************************/
+EXCEPT
+SELECT IdTipoIdentificacion, 
+       Descripcion
+FROM [dbo].[TipoIdentificacion]
+
+SET IDENTITY_INSERT [dbo].[TipoIdentificacion] OFF
+GO
--- TareaDatabase/Tables/TipoIdentificacion.sql
+++ TareaDatabase/Tables/TipoIdentificacion.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[TipoIdentificacion]
 (
 	[IdTipoIdentificacion] INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_TipoIdentificacion PRIMARY KEY CLUSTERED(IdTipoIdentificacion), 
-    [Descripcion] VARCHAR(50) NULL
+    [Descripcion] VARCHAR(20) NOT NULL
 )WITH (DATA_COMPRESSION = PAGE)
 GO
